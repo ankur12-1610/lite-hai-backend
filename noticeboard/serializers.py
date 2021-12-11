0cc16023f2858b7bedb3addb4e20e4ccec920f00
JG-11 <80846488+JeeveshGarg@users.noreply.github.com>
2021-12-11 10:40:17 +0530

    nit

--- noticeboard/serializers.py
+++ noticeboard/serializers.py
@@ -28,29 +28,29 @@ class NoticeDetailSerializer(serializers.ModelSerializer):
         fields = "__all__"
 
 
-# class NoticeCreateSerializer(serializers.ModelSerializer):
-#     def validate(self, attrs):
-#         request = self.context["request"]
-#         # pylint: disable=no-member
-#         profile = UserProfile.objects.get(user=request.user)
-#         print(profile.get_workshop_privileges())
-#         if profile.get_workshop_privileges():
-#             raise PermissionDenied("You are not authorized to create notices")
-#         return attrs
+class NoticeCreateSerializer(serializers.ModelSerializer):
+    def validate(self, attrs):
+        request = self.context["request"]
+        # pylint: disable=no-member
+        profile = UserProfile.objects.get(user=request.user)
+        print(profile.get_workshop_privileges())
+        if profile.get_workshop_privileges():
+            raise PermissionDenied("You are not authorized to create notices")
+        return attrs
 
-#     def save(self, **kwargs):
-#         data = self.validated_data
-#         # pylint: disable=no-member
-#         notice = NoticeBoard.objects.create(
-#             title=data["title"],
-#             description=data.get("description", ""),
-#             date=data.get("date", None),
-#             notice_url=data.get("link", None),
-#         )
-#         notice.save()
-#         # FirebaseAPI.send_entity_message(data, self.context['notices'])
-#         return notice
+    def save(self, **kwargs):
+        data = self.validated_data
+        # pylint: disable=no-member
+        notice = NoticeBoard.objects.create(
+            title=data["title"],
+            description=data.get("description", ""),
+            date=data.get("date", None),
+            notice_url=data.get("link", None),
+        )
+        notice.save()
+        # FirebaseAPI.send_entity_message(data, self.context['notices'])
+        return notice
 
-#     class Meta:
-#         model = NoticeBoard
-#         fields = ("title", "description", "date", "notice_url")
+    class Meta:
+        model = NoticeBoard
+        fields = ("title", "description", "date", "notice_url")
--- noticeboard/views.py
+++ noticeboard/views.py
@@ -1,6 +1,6 @@
 from rest_framework import generics
 from .models import NoticeBoard
-from .serializers import NoticeGetSerializer, NoticeCreateSerializer, NoticeDetailSerializer
+from .serializers import NoticeCreateSerializer, NoticeDetailSerializer
 from rest_framework import permissions
 from rest_framework import status
 from rest_framework.response import Response
